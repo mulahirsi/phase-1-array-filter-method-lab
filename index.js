9217778daf369ae3d934823a573fa12e4a1b2e38
mulahirsi <mula.hirsi@student.moringaschool.com>
2025-03-10 15:24:26 +0300

    array-filter-methods-lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+}
+
+module.exports = matchName;
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
+  
+  function fuzzyMatch(source, testString) {
+    return source.filter(
+      (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+  }
